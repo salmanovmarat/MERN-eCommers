201c255fc054fe5ddfa6a96a8f2d5fe72633104b
marat <marat.salmanov@wetravel.com>
2023-01-26 15:56:35 +0400

    user authentication endpoint and generate a JWT

---
+++ backend/controllers/userController.js
@@ -0,0 +1,21 @@
+import User from '../models/userModel.js';
+import asyncHandler from 'express-async-handler';
+import generatedToken from '../utils/generateToken.js';
+
+export const authUser = asyncHandler(async (req, res) => {
+  const { email, password } = req.body;
+  const user = await User.findOne({ email });
+
+  if (user && (await user.matchPassword(password))) {
+    res.json({
+      _id: user._id,
+      name: user.name,
+      email: user.email,
+      isAdmin: user.isAdmin,
+      token: generatedToken(user._id),
+    });
+  } else {
+    res.status(401);
+    throw new Error('Invalid email or password');
+  }
+});
