9dff4c9a35a5a110651fedede384e7f3392d65bc
marat <marat.salmanov@wetravel.com>
2023-01-27 12:41:03 +0400

    user login reducer and action

--- frontend/src/store.js
+++ frontend/src/store.js
@@ -7,17 +7,25 @@ import {
   productDetailsReducer,
 } from './reducers/productReducers';
 import { cartReducer } from './reducers/cartReducer';
+import { userLoginReducer } from './reducers/userReducer';
 
 const reducer = combineReducers({
   productList: productListReducer,
   productDetails: productDetailsReducer,
   cart: cartReducer,
+  userLogin: userLoginReducer,
 });
 const cartItemsFromLocalStorage = localStorage.getItem('cartItems')
   ? JSON.parse(localStorage.getItem('cartItems'))
   : [];
+
+const UserInfoFromLocalStorage = localStorage.getItem('userInfo')
+  ? JSON.parse(localStorage.getItem('userInfo'))
+  : null;
+
 const initialState = {
   cart: { cartItems: cartItemsFromLocalStorage },
+  userLogin: { userInfo: UserInfoFromLocalStorage },
 };
 const middleware = [thunk];
 
