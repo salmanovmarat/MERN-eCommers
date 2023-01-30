6dbab2363d990a8b06e028a20df6691aaa229d5e
marat <marat.salmanov@wetravel.com>
2023-01-30 11:15:50 +0400

    payment screen and save payment method

--- frontend/src/screens/ShippingScreen.js
+++ frontend/src/screens/ShippingScreen.js
@@ -25,6 +25,7 @@ const ShippingScreen = () => {
   return (
     <FormContainer>
       <CheckoutSteps step1 step2 />
+      <h1>Shipping</h1>
       <Form onSubmit={submitHandler}>
         <Form.Group controlId="address">
           <Form.Label>Address</Form.Label>
