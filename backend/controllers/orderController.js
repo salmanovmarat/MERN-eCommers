1dd0ca9b220cbc50f662a0d8c996bcb6765954b8
marat <marat.salmanov@wetravel.com>
2023-02-03 09:23:45 +0400

    add order details reducer, action and order screen

--- backend/controllers/orderController.js
+++ backend/controllers/orderController.js
@@ -31,4 +31,17 @@ const addOrderItems = asyncHandler(async (req, res) => {
   }
 });
 
-export { addOrderItems };
+const getOrderById = asyncHandler(async (req, res) => {
+  const order = await Order.findById(req.params.id).populate(
+    'user',
+    'name email'
+  );
+  if (order) {
+    return res.json(order);
+  } else {
+    res.status(404);
+    throw new Error('Order not found');
+  }
+});
+
+export { addOrderItems, getOrderById };
