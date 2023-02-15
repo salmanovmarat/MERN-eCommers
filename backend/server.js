5ac160c3dbc60bec8abc9e0e56e4084ffbf08faa
marat <marat.salmanov@wetravel.com>
2023-02-15 14:36:53 +0400

    image upload config and endpoint

--- backend/server.js
+++ backend/server.js
@@ -1,4 +1,5 @@
-import expess from 'express';
+import express from 'express';
+import path from 'path';
 import dotenv from 'dotenv';
 import color from 'colors';
 import connectDB from '../config/db.js';
@@ -6,13 +7,14 @@ import productRoutes from './routes/productRoutes.js';
 import { notFound, errorHandler } from './middleware/errorMiddleware.js';
 import userRoutes from './routes/userRoutes.js';
 import orderRoutes from './routes/orderRoutes.js';
+import uploadRoutes from './routes/uploadRoutes.js';
 
 dotenv.config();
 connectDB();
 
-const app = expess();
+const app = express();
 
-app.use(expess.json());
+app.use(express.json());
 
 app.get('/', (req, res) => {
   res.send('API is running');
@@ -21,10 +23,14 @@ app.get('/', (req, res) => {
 app.use('/api/products', productRoutes);
 app.use('/api/users', userRoutes);
 app.use('/api/orders', orderRoutes);
+app.use('/api/upload', uploadRoutes);
 
 app.get('/api/config/paypal', (req, res) =>
   res.send(process.env.PAYPAL_CLIENT_ID)
 );
+
+const __dirname = path.resolve();
+app.use('/uploads', express.static(path.join(__dirname, '/uploads')));
 
 app.use(notFound);
 app.use(errorHandler);
